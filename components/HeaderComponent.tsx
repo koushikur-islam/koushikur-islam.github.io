882ab2a00151be43dc5ce2d4b6a820a15daf3716
koushikur-webtech <koushikur.islam@gmail.com>
2025-01-20 00:35:12 +1100

    logo updated

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -14,15 +14,15 @@ export default function HeaderComponent() {
     return (
         <div className="fixed top-0 w-full z-40 backdrop-blur-3xl backdrop-saturate-10 backdrop-opacity-100 bg-dark/90 transition-colors border-b border-slate-700">
             <div className="limited-box flex justify-between">
-                <div className='ml-5 artistic-font pt-3 pb-1 overflow-hidden'>
+                <div className='ml-5 artistic-font pt-3 pb-3 overflow-hidden'>
                     <Link href='/' >
-                        <label style={{ color: 'var(--primary-color)', fontSize: 32, fontWeight:'bold', cursor:'pointer' }}>Koush</label>
+                        <Image src="/logo.png" width={50} height={25} alt='No logo available.' />
                     </Link>
                 </div>
                 <div className='inline-block sm:hidden mr-5 mt-6'>
                     <button onClick={showSidebar}><MenuOutlined style={{ fontSize: '20px', color: 'var(--primary-color)' }} /></button>
                 </div>
-                <div className='hidden sm:inline-block pt-5'>
+                <div className='hidden sm:inline-block pt-7'>
                     <div className="navbar">
                         <ul className="list-none">
                             <Link href='/'>
