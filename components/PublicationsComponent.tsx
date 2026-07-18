42001b69b6e9588710af0a75850ac033dc2fc8e6
koushikur-webtech <koushikur.islam@gmail.com>
2026-07-19 00:02:10 +1000

    update..

--- components/PublicationsComponent.tsx
+++ components/PublicationsComponent.tsx
@@ -9,16 +9,31 @@ export default function PublicationsComponent() {
             <ul className='px-5'>
                 {/* <li className='border-t border-slate-700 rounded pb-3'>
                     <div className='mt-3'>
-                        <p className='mr font-semibold text-lg'>Beyond SLAs: Drift-Aware Intent Assurance for Edge–Cloud Microservices Placement</p>
-                        <p className='my-2'> <label style={{ color: 'var(--primary-color)' }}>Koushikur Islam</label>, Jahedoul Anowar, Kanaka Sai Jagarlamudi, Manish Parashar, Rodrigo N. Calheiros</p>
-                        <p><label className='italic font-normal text-base'>The 19th IEEE International Conference on Utility and Cloud Computing (UCC 2026)</label></p>
+                        <p className='mr font-semibold text-lg'>Assurance Interference in Intent-Driven Management: Vision and Challenges</p>
+                        <p className='my-2'> <label style={{ color: 'var(--primary-color)' }}>Koushikur Islam</label>, Schahram Dustdar, Manish Parashar</p>
+                        <p><label className='italic font-normal text-base'>IEEE Internet Computing </label></p>
+                        <p className=' mb-3 mt-2'>DOI:
+                            <a className='underline mr-3' target='_blank' href="https://ieeexplore.ieee.org/document/11311632">10.1109/SmartIoT66867.2025.00026</a>
+                            <a className='underline' style={{ color: 'var(--primary-color)' }} target='_blank' rel='noopener noreferrer' href='publications/SmartIoT2025.pdf'>[PDF] </a>
+                        </p>
+                        <p className="research-item">IEEE</p>
+                        <p className="research-item">Magazine</p>
+                        <p className="research-item">Internet Computing</p>
+                    </div>
+                </li>
+
+                <li className='border-t border-slate-700 rounded pb-3'>
+                    <div className='mt-3'>
+                        <p className='mr font-semibold text-lg'>The Compliance Illusion: Runtime Intent Assurance for Edge–Cloud Microservices Placement</p>
+                        <p className='my-2'> <label style={{ color: 'var(--primary-color)' }}>Koushikur Islam</label>, Manish Parashar</p>
+                        <p><label className='italic font-normal text-base'>The 34th IEEE International Conference on Web Services (ICWS), UAE </label></p>
                         <p className=' mb-3 mt-2'>DOI:
                             <a className='underline mr-3' target='_blank' href="https://ieeexplore.ieee.org/document/11311632">10.1109/SmartIoT66867.2025.00026</a>
                             <a className='underline' style={{ color: 'var(--primary-color)' }} target='_blank' rel='noopener noreferrer' href='publications/SmartIoT2025.pdf'>[PDF] </a>
                         </p>
                         <p className="research-item">IEEE</p>
                         <p className="research-item">Conference</p>
-                        <p className="research-item">UCC</p>
+                        <p className="research-item">ICWS</p>
                     </div>
                 </li> */}
 
@@ -26,28 +41,17 @@ export default function PublicationsComponent() {
                     <div className='mt-3'>
                         <p className='mr font-semibold text-lg'>Intent Engine: Natural-Language Intent Translation for Intent-Driven Orchestration in the Compute Continuum </p>
                         <p className='my-2'> <label style={{ color: 'var(--primary-color)' }}>Koushikur Islam</label>, Rodrigo N. Calheiros</p>
-                        <p><label className='italic font-normal text-base'>Preprint</label></p>
+                        <p><label className='italic font-normal text-base'>Journal of Systems Architecture</label></p>
                         <p className=' mb-3 mt-2'>DOI:
                             <a className='underline mr-3' target='_blank' href="">TBD</a>
                             <a className='underline' style={{ color: 'var(--primary-color)' }} target='_blank' rel='noopener noreferrer' href='publications/IntentEngine.pdf'>[PDF] </a>
                         </p>
                         <p className="research-item">Elsevier</p>
                         <p className="research-item">Journal</p>
-                        <p className="research-item">Preprint</p>
+                        <p className="research-item">JSA</p>
                     </div>
                 </li>
 
-                <li className='border-t border-slate-700 rounded pb-3'>
-                    <div className='mt-3'>
-                        <p className='mr font-semibold text-lg'>A Quality and Privacy-Aware Edge–Cloud Continuum Framework for Internet of Things Applications </p>
-                        <p className='my-2'> Kanaka Sai Jagarlamudi, Jahedul Anowar, <label style={{ color: 'var(--primary-color)' }}>Koushikur Islam</label>, Fatemeh Mirhakimi, Rodrigo N. Calheiros, Bahman Javadi</p>
-                        <p><label className='italic font-normal text-base'>Internet of Things (IoT)</label></p>
-                        <p className=' mb-3 mt-2'>DOI: <a className='underline' target='_blank' href="https://doi.org/10.2139/ssrn.6754128">10.2139/ssrn.6754128</a> </p>
-                        <p className="research-item">Elsevier</p>
-                        <p className="research-item">Journal</p>
-                        <p className="research-item">IoT</p>
-                    </div>
-                </li>
 
                 <li className='border-t border-slate-700 rounded pb-3'>
                     <div className='mt-3'>
